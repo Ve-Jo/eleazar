ddb4ede7595a29c226531de543fb353ed6bda2f1
Ve-Jo <vejods@gmail.com>
2024-10-19 23:53:07 +0300

    subcommands cooldown feature

--- src/events/slash.js
+++ src/events/slash.js
@@ -1,25 +1,59 @@
-import { Events } from "discord.js";
+import { Events, Collection } from "discord.js";
 import i18n from "../utils/i18n";
+
+const cooldowns = new Collection();
 
 export default {
   name: Events.InteractionCreate,
   async execute(interaction) {
-    /*if (interaction.user.id !== "287275956744355841") {
-      return interaction.reply({
-        content: "...",
-      });
-    }*/
-
     if (!interaction.isChatInputCommand()) return;
 
-    const command = interaction.client.commands.get(interaction.commandName);
+    const commandName = interaction.commandName;
+    const subcommandName = interaction.options.getSubcommand(false);
+    const fullCommandName = subcommandName
+      ? `${commandName}_${subcommandName}`
+      : commandName;
+
+    const command = interaction.client.commands.get(commandName);
 
     if (!command) return;
 
-    // Add a check for command description
     if (!command.data.description) {
       console.error(`Command "${command.data.name}" is missing a description.`);
       return;
+    }
+
+    let cooldownAmount;
+    if (subcommandName && command[subcommandName]) {
+      cooldownAmount = command[subcommandName].cooldown;
+    }
+
+    if (cooldownAmount) {
+      if (!cooldowns.has(fullCommandName)) {
+        cooldowns.set(fullCommandName, new Collection());
+      }
+
+      const now = Date.now();
+      const timestamps = cooldowns.get(fullCommandName);
+      const cooldownMs = cooldownAmount * 1000;
+
+      if (timestamps.has(interaction.user.id)) {
+        const expirationTime = timestamps.get(interaction.user.id) + cooldownMs;
+
+        if (now < expirationTime) {
+          const timeLeft = (expirationTime - now) / 1000;
+          return interaction.reply({
+            content: `Please wait ${timeLeft.toFixed(
+              1
+            )} more second(s) before reusing the \`${fullCommandName}\` command.`,
+            ephemeral: true,
+          });
+        }
+      }
+
+      timestamps.set(interaction.user.id, now);
+      console.log(timestamps);
+      setTimeout(() => timestamps.delete(interaction.user.id), cooldownMs);
     }
 
     try {
