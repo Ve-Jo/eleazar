f5b8f8f10350cde6b99dfb365231853c4245b8b0
Ve-Jo <vejods@gmail.com>
2024-10-27 16:52:18 +0200

    better queue visibility

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -262,7 +262,7 @@ const MusicPlayer = ({
           left: 0,
           right: 0,
           bottom: 0,
-          backgroundColor: "rgba(0, 0, 0, 0.2)",
+          backgroundColor: "rgba(0, 0, 0, 0.4)",
         }}
       />
 
