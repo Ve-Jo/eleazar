c9aa43ab32e9f652a4a6cc95919de9a3a3c9db82
Ve-Jo <vejods@gmail.com>
2025-04-08 00:27:14 +0300

    removing custom builders, now using more classical way

--- src/utils/builders/index.js
+++
@@ -1,8 +0,0 @@
-export { BaseCommandBuilder } from "./BaseCommandBuilder.js";
-export { CommandBuilder } from "./CommandBuilder.js";
-export { I18nCommandBuilder } from "./I18nCommandBuilder.js";
-export { OptionType } from "./OptionTypes.js";
-export { SlashCommand } from "./SlashCommand.js";
-export { SlashCommandOption } from "./SlashCommandOption.js";
-export { SlashCommandSubcommand } from "./SlashCommandSubcommand.js";
-export { SubcommandBuilder } from "./SubcommandBuilder.js";
