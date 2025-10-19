899c936277e63e1d5f08177fc6b2616c60566fbf
Ve-Jo <vejods@gmail.com>
2025-10-20 00:01:22 +0300

    new two migrations

---
+++ hub/database/prisma/migrations/20251017215302_personalization/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "age" INTEGER,
+ADD COLUMN     "country_code" TEXT,
+ADD COLUMN     "gender" TEXT,
+ADD COLUMN     "pronouns" TEXT,
+ADD COLUMN     "real_name" TEXT;
